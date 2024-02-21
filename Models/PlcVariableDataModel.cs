58e3017434ccb6341a7539f4a090519a5ff46bea
Fereshte Sotoudeh <111074997+Fereshteh-Sotoudeh@users.noreply.github.com>
2024-02-21 09:58:15 +0300

    Update PlcVariableDataModel.cs
    Code review: Removed 'VarType' property

--- Models/PlcVariableDataModel.cs
+++ Models/PlcVariableDataModel.cs
@@ -8,7 +8,7 @@ namespace CodingTask
         public MitsuRVariables Variable { get; set; }
         public object Value { get; set; }
         public string Description { get; set; }
-        public Type VarType { get; set; }
+  
     }
 
 }
